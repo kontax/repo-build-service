421c7ac7ca57228fb1f79f004273bad8cd2ded62
James Coulson <github@coulson.ie>
2019-05-03 08:02:09 +0100

    Use threading and dynamodb batch within package updater

--- package_updater/update_packages.py
+++ package_updater/update_packages.py
@@ -4,6 +4,8 @@ import os
 from arch_packages import get_packages
 from aws import get_dynamo_resource
 from best_mirror import get_best_mirror
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
 
 PACKAGE_TABLE = os.environ.get('PACKAGE_TABLE')
 COUNTRIES = os.environ.get('COUNTRIES')
@@ -23,14 +25,21 @@ def lambda_handler(event, context):
     table = dynamo.Table(PACKAGE_TABLE)
 
     # Get any new packages that have been added to the repositories and upload them
+    print("Getting all current items in the table")
     resp = table.scan()
     response_body['current'] = len(resp['Items'])
+
+    print("Adding new packages")
     new_packages = add_new_packages(mirrors, table)
-    response_body['new'] = len(new_packages) - response_body['current']
+    new_diff = len(new_packages) - response_body['current']
+    response_body['new'] = new_diff if new_diff > 0 else 0
 
     # Remove any packages no longer contained within the repositories
+    print("Getting all current and new items in the table")
     resp = table.scan()
     all_packages = [x['PackageName'] for x in resp['Items']]
+
+    print("Removing old packages")
     to_delete = delete_old_packages(all_packages, new_packages, table)
     response_body['deleted'] = len(to_delete)
 
@@ -50,11 +59,16 @@ def add_new_packages(mirrors, table):
         list: A collection of all packages within the mirrors.
     """
     new_packages = []
-    for mirror in mirrors:
-        packages = get_packages(mirror)
-        for package in packages:
-            table.update_item(Key={'PackageName': package})
-            new_packages.append(package)
+    with ThreadPoolExecutor() as executor:
+        new_packages.extend([x for x in executor.map(get_packages, mirrors)])
+
+    # ThreadPoolExecutors returns a list of lists, which needs to be unwrapped here
+    new_packages = [item for sublist in new_packages for item in sublist]
+
+    with table.batch_writer(overwrite_by_pkeys=['PackageName']) as batch:
+        for i in new_packages:
+            batch.put_item(Item={'PackageName': i})
+
     return new_packages
 
 
@@ -70,8 +84,9 @@ def delete_old_packages(all_packages, new_packages, table):
         list: The collection of packages removed from the database
     """
     to_delete = set(all_packages).difference(new_packages)
-    for package in to_delete:
-        table.delete_item(Key={'PackageName': package})
+    with table.batch_writer() as batch:
+        for package in to_delete:
+            batch.delete_item(Key={'PackageName': package})
     return to_delete
 
 
