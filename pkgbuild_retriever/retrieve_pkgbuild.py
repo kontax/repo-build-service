c8c433d4b82a5e0a3c54d3f6282c1f1a59dba6d5
James Coulson <github@coulson.ie>
2020-04-28 17:59:17 +0100

    Check stage name matches the pushed branch

--- pkgbuild_retriever/retrieve_pkgbuild.py
+++ pkgbuild_retriever/retrieve_pkgbuild.py
@@ -1,28 +1,36 @@
 import json
 import os
+import requests
 from urllib.parse import unquote
 
-from botocore.vendored import requests
-
-import commit_parser
 import github_token_validator
 from aws import invoke_lambda
 from common import return_code
 
 NEXT_FUNC = os.environ.get("NEXT_FUNC")
+STAGE_NAME = os.environ.get("STAGE_NAME")
 
 
 def lambda_handler(event, context):
     print(event)
-    
+    branch_check = 'master' if STAGE_NAME == 'prod' else STAGE_NAME
+
     # Validate github token
     response = github_token_validator.validate(event)
     if response['statusCode'] != 200:
         return response
 
     commit_payload = json.loads(unquote(event['body']).replace("payload=", ""))
-    full_name = commit_parser.get_full_name(commit_payload)
-    pkgbuild_location = commit_parser.get_pkgbuild_location(commit_payload)
+    full_name = get_full_name(commit_payload)
+
+    branch = commit_payload['ref'].replace('refs/heads/', '')
+    if branch_check != branch:
+        return return_code(401, 
+            {'headers': {'Content-Type': 'text/plain'}, 
+            'body': f'branch name {branch_check} does not match stage {branch}'}
+        )
+
+    pkgbuild_location = get_pkgbuild_location(commit_payload)
     if pkgbuild_location is None:
         print("No PKGBUILD commit found, exiting")
         return return_code(401, {'headers': {'Content-Type': 'text/plain'}, 'body': 'No updated PKGBUILD found'})
@@ -38,3 +46,18 @@ def lambda_handler(event, context):
     print(response)
 
     return return_code(200, {'status': 'PKGBUILD extracted'})
+
+
+def get_pkgbuild_location(payload):
+    if 'commits' not in payload:
+        return None
+
+    pkgbuild = [x for x in payload['commits'][0]['modified'] if x.endswith("PKGBUILD")]
+    if len(pkgbuild) == 0:
+        return None
+
+    return pkgbuild[0]
+
+
+def get_full_name(payload):
+    return payload['repository']['full_name']
