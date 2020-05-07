d73fb0098f4a985134488c1e81a5c70c5e5c5460
James Coulson <github@coulson.ie>
2020-05-07 12:20:47 +0100

    Change comment type and input SQS message for fanout starter

--- fanout_starter/starter.py
+++ fanout_starter/starter.py
@@ -13,30 +13,43 @@ PERSONAL_REPO = os.environ.get('PERSONAL_REPO')
 
 def lambda_handler(event, context):
     print(event)
-    # Get Dependencies
-    deps = event['dependencies']
-    url = event['url']
 
     # The dynamoDB table containing the running status of each package
     dynamo = get_dynamo_resource()
     package_table = dynamo.Table(PACKAGE_TABLE)
 
-    # Put each one in the FANOUT_STATUS table with an "Initialized" status and add it to the queue
-    build_packages = get_packages_to_build(package_table, deps)
-    process_packages(build_packages, url)
+    for record in event['Records']:
+
+        json_record = json.loads(record)
+        # Get Dependencies
+        deps = json_record['dependencies']
+        git_url = json_record['git_url']
+        git_branch = json_record['git_branch']
+        stage = json_record['stage']
+
+        # Put each one in the FANOUT_STATUS table with an "Initialized"
+        # status and add it to the queue
+        build_packages = get_packages_to_build(package_table, deps)
+        process_packages(build_packages, git_url, git_branch, stage)
 
     return return_code(200, {'packages': build_packages})
 
 
 def get_packages_to_build(package_table, pkgbuild_packages):
-    """ Compare the packages contained within the PKGBUILD to those already available in various repositories,
-    returning only those which need to be built.
+    """ Compare the packages contained within the PKGBUILD to those already
+    available in various repositories, returning only those which need to be
+    built.
 
-    :param (Table) package_table: The table containing all packages already available.
-    :param (list) pkgbuild_packages: The collection of packages to check against those available.
-    :return: A list of packages to send to the build queue.
-    :rtype: list
+    Args:
+        package_table (Table):    The table containing all packages already
+                                  available.
+        pkgbuild_packages (list): The collection of packages to check against
+                                  those available.
+
+    Returns:
+        (list): A list of packages to send to the build queue.
     """
+
     print("Getting all current and new items in the table")
     resp = package_table.scan()
     all_packages = [x['PackageName'] for x in resp['Items']]
@@ -45,10 +58,14 @@ def get_packages_to_build(package_table, pkgbuild_packages):
 
 
 def process_packages(build_packages, metapackage_url):
-    """ Add packages to be built to a build queue including the metapackage URL for building after completion.
+    """ Add packages to be built to a build queue including the metapackage
+    URL for building after completion.
 
-    :param (list) build_packages: The collection of packages to build as a list of package names
-    :param (str) metapackage_url: The URL of the repository for the metapackage to build after the rest
+    Args:
+        build_packages (list): The collection of packages to build as a list 
+                               of package names
+        metapackage_url (str): The URL of the repository for the metapackage 
+                               to build after the rest
     """
 
     # Build the other packages
@@ -66,16 +83,23 @@ def process_packages(build_packages, metapackage_url):
 
 
 def process_package(package):
-    """Adds packages to the build queue and updates their status
+    """ Adds packages to the build queue and updates their status
 
-    :param (str) package: The package to check
+    Args:
+        package (str): The package to check
     """
+
     print(f"Building package: {package}")
 
     # Update the status to say the package is building
-    message = {"PackageName": package, "BuildStatus": Status.Building.name, "IsMeta": False}
+    message = {
+        "PackageName": package,
+        "BuildStatus": Status.Building.name,
+        "IsMeta": False
+    }
     send_to_queue(FANOUT_QUEUE, json.dumps(message))
 
     # Add them to the build queue and start the build VM
-    send_to_queue(BUILD_FUNCTION_QUEUE, {"PackageName": package, "Repo": PERSONAL_REPO})
+    build_msg = {"PackageName": package, "Repo": PERSONAL_REPO}
+    send_to_queue(BUILD_FUNCTION_QUEUE, build_msg)
 
