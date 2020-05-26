ae35eab8d2a21793c60d4eae4c902664d4e735e6
James Coulson <github@coulson.ie>
2020-05-26 23:55:50 +0100

    Fix issue with pkg_builder sending full queue message instead of just data

--- pkg_builder/build_package.py
+++ pkg_builder/build_package.py
@@ -13,10 +13,10 @@ MAX_TASK_COUNT = int(os.environ.get('MAX_TASK_COUNT'))
 
 def lambda_handler(event, context):
     print(json.dumps(event))
-    package_dict = event
 
     # Send the package to the build queue for any ECS instances to consume
-    send_to_queue(BUILD_QUEUE, json.dumps(package_dict))
+    for package_dict in event['Records']:
+        send_to_queue(BUILD_QUEUE, package_dict['body'])
 
     # Start a new task if it's less than the max required
     if get_running_task_count(ECS_CLUSTER, TASK_FAMILY) < MAX_TASK_COUNT:
