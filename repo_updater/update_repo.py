c313afdf3e98f22dfd3db5cd1aa17d6ddb2117a2
kontax <github@coulson.ie>
2020-06-15 22:09:26 +0100

    Update both prod and dev buckets (#17)

--- repo_updater/update_repo.py
+++ repo_updater/update_repo.py
@@ -6,9 +6,25 @@ from common import return_code
 
 ECS_CLUSTER = os.environ.get('ECS_CLUSTER')
 TASK_DEFN = os.environ.get('TASK_DEFN')
+REPO_ARCH = os.environ.get('REPO_ARCH')
+PERSONAL_REPO_BUCKET = os.environ.get('PERSONAL_REPO_BUCKET')
+DEV_REPO_BUCKET = os.environ.get('DEV_REPO_BUCKET')
 
 
 def lambda_handler(event, context):
     print(json.dumps(event))
-    start_ecs_task(ECS_CLUSTER, TASK_DEFN)
+    start_ecs_task(ECS_CLUSTER, TASK_DEFN, get_env_overrides(PERSONAL_REPO_BUCKET))
+    start_ecs_task(ECS_CLUSTER, TASK_DEFN, get_env_overrides(DEV_REPO_BUCKET))
     return return_code(200, {'status': 'Repository updating'})
+
+
+def get_env_overrides(bucket):
+    return {
+        'containerOverrides': [{
+            'name': 'aur-pkg-update',
+            'environment': [{
+                'name': 'REMOTE_PATH',
+                'value': f's3://{bucket}/{REPO_ARCH}'
+            }]
+        }]
+    }
